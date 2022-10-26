f176b848b278bd5e5d97cae1700cdc7b1c8bc8ab
hupg <1360326757@qq.com>
2022-10-26 17:43:50 +0800

    接口层增加接口

---
+++ core-api/src/main/java/io/github/dingdangdog/runner/DbInitRunner.java
@@ -0,0 +1,35 @@
+package io.github.dingdangdog.runner;
+
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
+
+/**
+ * 自定义数据库初始化逻辑的接口
+ *
+ * @author DingDangDog
+ * @since 2022/5/10 16:36
+ */
+public interface DbInitRunner extends ApplicationRunner {
+
+    /**
+     * 执行初始化的入口
+     *
+     * @throws Exception 任何异常
+     * @author DingDangDog
+     * @since 2022/10/26 17:20
+     */
+    void init() throws Exception;
+
+    /**
+     * runner的run方法默认实现
+     *
+     * @param args 服务环境
+     * @throws Exception 任何异常
+     * @author DingDangDog
+     * @since 2022/10/26 17:25
+     */
+    @Override
+    default void run(ApplicationArguments args) throws Exception {
+        this.init();
+    }
+}
